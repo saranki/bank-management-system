aec26eba1fe612f642bd3425bfa5fe51a9887fd9
Saranki Magenthirarajah <m.saranki@gmail.com>
2020-03-24 19:09:06 +0530

    Created the class mediator to retrieve role claim

--- Project/tokenMediator/src/main/java/com/demo/JWTTokenMediator.java
+++ Project/tokenMediator/src/main/java/com/demo/JWTTokenMediator.java
@@ -1,35 +1,36 @@
 package com.demo;
 
-import java.util.Base64;
-
+import org.apache.commons.codec.binary.Base64;
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.mediators.AbstractMediator;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import jdk.nashorn.internal.parser.JSONParser;
 
 public class JWTTokenMediator extends AbstractMediator {
 
 	private static final Logger log = LoggerFactory.getLogger(JWTTokenMediator.class);
-	private String jwtHeader;
+	private static final String CLAIMS = "http://wso2.org/claims/role";
+	private static final String PROPERTY = "role";
 	private static String role;
+	private String jwtHeader;
 
 	private static String retrieveRole(String accountRequestInfo) {
-		String[] splitString = accountRequestInfo.split("\\.");
-		String base64EncodedBody = splitString[1];
-
+		String splitString = accountRequestInfo.split("\\.")[1];
 		Base64 base64 = new Base64();
 		try {
-			String decodedString = new String(base64.decode(base64EncodedBody.getBytes()));
+			String decodedString = new String(base64.decode(splitString.getBytes()));
 			JSONParser parser = new JSONParser();
-			JSONObject accountRequestInfoJson = (JSONObject) parser.parse(decodedString);
-			if (accountRequestInfoJson.containsKey("http://wso2.org/claims/role")) {
+			JSONObject requestInfo = (JSONObject) parser.parse(decodedString);
 
-				role = accountRequestInfoJson.get("http://wso2.org/claims/role").toString();
+			if (requestInfo.containsKey(CLAIMS)) {
+				role = requestInfo.get(CLAIMS).toString();
 				return role;
 			} else {
 				if (log.isDebugEnabled())
-					log.error("external id is not available");
+					log.error("Role is not available!");
 			}
 		} catch (ParseException e) {
 			log.error("Error in passing Account-Request-Information " + e.toString());
@@ -38,17 +39,22 @@ public class JWTTokenMediator extends AbstractMediator {
 	}
 
 	public boolean mediate(MessageContext context) {
-
-		context.setProperty("role", retrieveRole(getJWT_HEADER()));
+		 String jwtValue = getJWTHeader();
+		 String retrievedRole = retrieveRole(jwtValue);
+		 if(retrievedRole == null) {
+			 context.setProperty(PROPERTY, jwtValue);
+		 return false;
+		 }
+		 context.setProperty(PROPERTY, retrievedRole);
 
 		return true;
 	}
 
-	public String getJWT_HEADER() {
+	public String getJWTHeader() {
 		return jwtHeader;
 	}
 
-	public void setJWT_HEADER(String jwtHeader) {
+	public void setJWTHeader(String jwtHeader) {
 		this.jwtHeader = jwtHeader;
 	}
 }
